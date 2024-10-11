7ead492cb46c007074df0ed682ebabd529e429a5
Chathura Theekshana <chathuradayananda1@gmail.com>
2024-10-11 21:12:27 +0530

    redirection added

--- swiftbus/lib/SeatStructure/screens/bus_seat_layout_overview.dart
+++ swiftbus/lib/SeatStructure/screens/bus_seat_layout_overview.dart
@@ -1,63 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore package
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/bus_model.dart';
 import '../widgets/seat_layout.dart';
 import '../widgets/legend.dart';
 import 'disable_seats_screen.dart';
-import 'seat_selection_screen.dart';
 
 class BusSeatLayoutOverview extends StatefulWidget {
   final Map<String, dynamic> busDetails;
+  final BusModel busModel;
 
-  BusSeatLayoutOverview({required this.busDetails, required BusModel busModel});
+  const BusSeatLayoutOverview({
+    Key? key,
+    required this.busDetails,
+    required this.busModel,
+  }) : super(key: key);
 
   @override
   _BusSeatLayoutOverviewState createState() => _BusSeatLayoutOverviewState();
 }
 
 class _BusSeatLayoutOverviewState extends State<BusSeatLayoutOverview> {
-  Set<int> disabledSeats = Set();
-  late BusModel busModel;
-
-  @override
-  void initState() {
-    super.initState();
-    // Create a BusModel instance from the busDetails
-    busModel = BusModel(
-      id: widget.busDetails['busNo'],
-      name: widget.busDetails['busName'] ?? 'Bus ${widget.busDetails['busNo']}',
-      imageUrl: widget.busDetails['imageUrls']?.isNotEmpty == true
-          ? widget.busDetails['imageUrls'][0]
-          : '/api/placeholder/300/200', // Use the first image URL if available, otherwise use a placeholder
-      seatMap: _createSeatMap(widget.busDetails['seatFormat']),
-      totalSeats: _calculateTotalSeats(widget.busDetails['seatFormat']),
-    );
-  }
-
-  List<List<int?>> _createSeatMap(String seatFormat) {
-    int seatsPerRow = seatFormat == '4 seater' ? 4 : 5;
-    int rows = 10; // Assuming 10 rows, adjust as needed
-    return List.generate(rows, (row) {
-      return List.generate(seatsPerRow + 1, (col) {
-        if (col == 2) return null; // Add null for aisle
-        int seatNumber = row * seatsPerRow + (col > 2 ? col - 1 : col) + 1;
-        return seatNumber <= _calculateTotalSeats(seatFormat)
-            ? seatNumber
-            : null;
-      });
-    });
-  }
-
-  int _calculateTotalSeats(String seatFormat) {
-    int seatsPerRow = seatFormat == '4 seater' ? 4 : 5;
-    return seatsPerRow * 10; // Assuming 10 rows, adjust as needed
-  }
+  Set<int> disabledSeats = {};
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(busModel.name),
+        title: Text(widget.busModel.name),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -66,7 +35,7 @@ class _BusSeatLayoutOverviewState extends State<BusSeatLayoutOverview> {
           children: [
             Expanded(
               child: SeatLayout(
-                seatMap: busModel.seatMap,
+                seatMap: widget.busModel.seatMap,
                 disabledSeats: disabledSeats,
                 onSeatTap: null,
                 seatColor: (seatNumber) => disabledSeats.contains(seatNumber)
@@ -88,28 +57,34 @@ class _BusSeatLayoutOverviewState extends State<BusSeatLayoutOverview> {
                     child: Text(
                       'Disable Seats',
                       style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.orange,
                       minimumSize: Size(double.infinity, 50),
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
                     ),
                   ),
                   SizedBox(height: 16),
                   ElevatedButton(
-                    onPressed: _completeBooking, // Call the function here
+                    onPressed: _completeBooking,
                     child: Text(
                       'Complete',
                       style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
                       minimumSize: Size(double.infinity, 50),
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
                     ),
                   ),
                 ],
@@ -121,14 +96,13 @@ class _BusSeatLayoutOverviewState extends State<BusSeatLayoutOverview> {
     );
   }
 
-  // Function to navigate to disable seats screen
   void _goToDisableSeatsScreen() async {
     final result = await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => DisableSeatsScreen(
           initialDisabledSeats: disabledSeats,
-          busModel: busModel,
+          busModel: widget.busModel,
         ),
       ),
     );
@@ -139,28 +113,22 @@ class _BusSeatLayoutOverviewState extends State<BusSeatLayoutOverview> {
     }
   }
 
-  // Function to save bus details to Firebase
   Future<void> _completeBooking() async {
     try {
-      await FirebaseFirestore.instance.collection('buses').add({
-        'busNo': widget.busDetails['busNo'],
-        'conducterId': widget.busDetails['conducterId'],
-        'busName': widget.busDetails['busName'],
-        'busType': widget.busDetails['busType'],
-        'startLocation': widget.busDetails['startLocation'],
-        'destination': widget.busDetails['destination'],
-        'departureTime': widget.busDetails['departureTime'],
-        'departureDate': widget.busDetails['departureDate'],
-        'price': widget.busDetails['price'],
-        'seatFormat': widget.busDetails['seatFormat'],
-        'tripPoints': widget.busDetails['tripPoints'],
-        'imageUrls': widget.busDetails['imageUrls'],
-        'disabledSeats': disabledSeats.toList(), // Saving the disabled seats
-      });
+      final Map<String, dynamic> finalBusDetails = {
+        ...widget.busDetails,
+        'disabledSeats': disabledSeats.toList(),
+        'totalSeats': widget.busModel.totalSeats,
+        'seatMap': widget.busModel.seatMap,
+      };
+
+      await FirebaseFirestore.instance.collection('buses').add(finalBusDetails);
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Bus details saved successfully!')),
       );
-      Navigator.pop(context); // Return to the previous screen
+
+      Navigator.popUntil(context, (route) => route.isFirst);
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error saving bus details: $e')),
--- swiftbus/lib/busRegistration/busRegistrationPage.dart
+++ swiftbus/lib/busRegistration/busRegistrationPage.dart
@@ -1,112 +1,274 @@
-import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:swiftbus/busRegistration/finalbusregpage.dart';
-import 'package:swiftbus/busRegistration/intermediatebusreg.dart';
-// import 'package:swiftbus/busRegistration/busRegistraionWidget.dart';
-import 'package:swiftbus/busRegistration/widgets/initialBusregwidget.dart';
+import 'package:path/path.dart' as path;
+import 'package:image_picker/image_picker.dart';
+import '../SeatStructure/models/bus_model.dart';
+import '../SeatStructure/screens/bus_seat_layout_overview.dart';
 
-class Busregistration extends StatefulWidget {
-  const Busregistration({super.key});
+class Finalbusregpage extends StatefulWidget {
+  final Map<String, dynamic> busDetails;
+  final Function(Map<String, dynamic>) onNext;
+
+  const Finalbusregpage({
+    Key? key,
+    required this.busDetails,
+    required this.onNext,
+  }) : super(key: key);
 
   @override
-  State<Busregistration> createState() => _BusregistrationState();
+  State<Finalbusregpage> createState() => _FinalbusregpageState();
 }
 
-class _BusregistrationState extends State<Busregistration> {
+class _FinalbusregpageState extends State<Finalbusregpage> {
+  List<XFile> _images = [];
+  bool _isUploading = false;
+  double _uploadProgress = 0.0;
 
-  final PageController _pageController = PageController();
-  Map<String, dynamic> busDetails = {};
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFiles = await picker.pickMultiImage();
+    setState(() {
+      _images.addAll(pickedFiles);
+    });
+  }
 
+  void _removeImage(int index) {
+    setState(() {
+      _images.removeAt(index);
+    });
+  }
+
+  Future<void> _uploadImages() async {
+    setState(() {
+      _isUploading = true;
+      _uploadProgress = 0.0;
+    });
+
+    List<String> uploadedUrls = [];
+    try {
+      for (int i = 0; i < _images.length; i++) {
+        XFile image = _images[i];
+        String filename = path.basename(image.path);
+        String fileExtension = path.extension(filename);
+        final ref = FirebaseStorage.instance.ref().child('bus_images').child(
+            '${widget.busDetails['busNo']}_${DateTime.now().millisecondsSinceEpoch}$fileExtension');
+
+        UploadTask uploadTask = ref.putFile(File(image.path));
+
+        uploadTask.snapshotEvents.listen((TaskSnapshot snapshot) {
+          setState(() {
+            _uploadProgress =
+                (i + (snapshot.bytesTransferred / snapshot.totalBytes)) /
+                    _images.length;
+          });
+        });
+
+        await uploadTask;
+        String downloadUrl = await ref.getDownloadURL();
+        uploadedUrls.add(downloadUrl);
+      }
+
+      // Add image URLs to bus details
+      widget.busDetails['imageUrls'] = uploadedUrls;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Images uploaded successfully')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error uploading images: $e')),
+      );
+    } finally {
+      setState(() {
+        _isUploading = false;
+        _uploadProgress = 0.0;
+      });
+    }
+  }
+
+  List<List<int?>> _createSeatMap(String seatFormat) {
+    int seatsPerRow = seatFormat == '4 seater' ? 4 : 5;
+    int rows = 10;
+    return List.generate(rows, (row) {
+      return List.generate(seatsPerRow + 1, (col) {
+        if (col == 2) return null; // Add null for aisle
+        int seatNumber = row * seatsPerRow + (col > 2 ? col - 1 : col) + 1;
+        return seatNumber <= _calculateTotalSeats(seatFormat)
+            ? seatNumber
+            : null;
+      });
+    });
+  }
+
+  int _calculateTotalSeats(String seatFormat) {
+    return seatFormat == '4 seater' ? 40 : 50;
+  }
+
+  Future<void> _handleNext() async {
+    if (_images.isNotEmpty && !widget.busDetails.containsKey('imageUrls')) {
+      await _uploadImages();
+    }
+
+    // Create a BusModel instance
+    final busModel = BusModel(
+      id: widget.busDetails['busNo'],
+      name: widget.busDetails['busName'] ?? 'Bus ${widget.busDetails['busNo']}',
+      imageUrl: widget.busDetails['imageUrls']?.isNotEmpty == true
+          ? widget.busDetails['imageUrls'][0]
+          : null,
+      seatMap: _createSeatMap(widget.busDetails['seatFormat']),
+      totalSeats: _calculateTotalSeats(widget.busDetails['seatFormat']),
+    );
+
+    // Navigate to BusSeatLayoutOverview
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => BusSeatLayoutOverview(
+          busDetails: widget.busDetails,
+          busModel: busModel,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        statusBarColor: Colors.orange,
-        statusBarIconBrightness: Brightness.light));
-    return Scaffold(
-      body:SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-        color: Colors.white,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Column(
-              children: [
-                //top arc
-                Container(
-                  height: 80,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.elliptical(
-                            (MediaQuery.of(context).size.width), 90),
-                        bottomRight: Radius.elliptical(
-                            (MediaQuery.of(context).size.width), 90)),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Register your bus",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.black),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-
-                SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: PageView(
-                    controller: _pageController,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      InitialBusDetailsPage(
-                        onNext: (initialDetails){
-                          setState(() {
-                            busDetails = initialDetails;
-                            _pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
-                          });
-                        }
-                      ),
-                      // Intermediatebusreg(initialDetails: initialDetails),
-                      Intermediatebusreg(
-                        initialDetails: busDetails,
-                        onNext: (intermediateDetails){
-                          setState(() {
-                            busDetails = intermediateDetails;
-                          });
-                          _pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
-                        }
-                      ),
-
-                      // FinalBusDetailsPage(initialDetails: initialDetails),
-                      Finalbusregpage(
-                        busDetails: busDetails,
-                        onNext: (intermediateDetails){
-                          setState(() {
-                            busDetails = intermediateDetails;
-                          });
-                          _pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn);
-                        }
-                      )
-                    ],
-                  ),
-                )
-                
-              ],
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 3, color: const Color.fromARGB(255, 71, 145, 2)),
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+          color: Colors.green[200],
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 80, 80, 80).withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 20,
+              offset: const Offset(0, 8),
             ),
           ],
         ),
-      )),
-      ) 
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Add images of bus No: ${widget.busDetails['busNo']} (Optional)\nUploaded images will help passengers identify your bus easily.',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _pickImage,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                      ),
+                      child: const Text(
+                        'Add Images',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    if (_images.isNotEmpty) ...[
+                      SizedBox(height: 10),
+                      Text('${_images.length} images selected'),
+                      SizedBox(height: 10),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: _images.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                        ),
+                        itemBuilder: (context, index) {
+                          return Stack(
+                            children: [
+                              Image.file(
+                                File(_images[index].path),
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: IconButton(
+                                  onPressed: () => _removeImage(index),
+                                  icon: const Icon(
+                                    Icons.cancel,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                    SizedBox(height: 20),
+                    if (_isUploading)
+                      LinearProgressIndicator(value: _uploadProgress),
+                    SizedBox(height: 20),
+                    if (_images.isNotEmpty)
+                      ElevatedButton(
+                        onPressed: _isUploading ? null : _uploadImages,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 15,
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.upload,
+                          size: 30,
+                          color: _isUploading ? Colors.grey : Colors.green,
+                        ),
+                      ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: _isUploading ? null : _handleNext,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 27, 117, 30),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 20,
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.arrow_forward,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
